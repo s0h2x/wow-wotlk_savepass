e1a4fecc40c1051fb066297d303a3793f6f54446
s0high <33549022+s0h2x@users.noreply.github.com>
2022-04-10 03:49:52 +1000

    Add files via upload

---
+++ Interface/GlueXML/OptionsSelect.lua
@@ -0,0 +1,20 @@
+
+function OptionsSelectFrame_Hide()
+	PlaySound("gsLoginChangeRealmCancel");
+	OptionsSelectFrame:Hide();
+end
+
+function OptionsSelectResetSettingsButton_OnClick_Reset(self)
+	PlaySound("igMainMenuOptionCheckBoxOn");
+	GlueDialog_Show("RESET_SERVER_SETTINGS");
+end
+
+function OptionsSelectResetSettingsButton_OnClick_Cancel(self)
+	PlaySound("igMainMenuOptionCheckBoxOn");
+	GlueDialog_Show("CANCEL_RESET_SETTINGS");
+end
+
+function AccountLogin_RealmReset()
+	SetCVar("realmName", "");
+	AccountLogin_Exit();
+end
